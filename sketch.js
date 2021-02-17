918a8cbfc6abeee3837ca83efd4636f69d527a35
rakshit-parikh <71026422+rakshit-parikh@users.noreply.github.com>
2021-02-17 19:05:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var gameState=0;
+var playerCount
+var form,player,game;
+var database;
+var distance=0;
+var allPlayers
+function setup(){
+   canvas= createCanvas(400,400);
+   database=firebase.database()
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+   if(playerCount===4){game.update(1)}
+  if(gameState===1)  {
+     clear()
+     game.play()
+  }
+}
